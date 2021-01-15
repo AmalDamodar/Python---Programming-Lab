56ed470ff72864f7cd25c4a21a0b19500168f7a5
Amal Damodar <amhzamal@gmail.com>
2021-01-15 12:21:06 +0530

    Updated

---
+++ pass_gen.py
@@ -0,0 +1,14 @@
+import random
+
+lower="abcdefghijklmnopqrstuvwxyz"
+upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number="0123456789"
+symbols="!@#$%^&*()_+:?~"
+
+all = lower+upper+number+symbols
+
+length=8
+password= "".join(random.sample(all,length))
+print("PASSWORD GENERATOR")
+print("++++++++++++++++++")
+print(password)
